ff7d1bab8e2ca5fb9fab5c5316b8e8b6388c1de5
BehindYou™ <111340893+b3h1ndy0u@users.noreply.github.com>
2025-01-08 01:56:12 +0200

    Create include.php

---
+++ include.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once "system/idiorm.php";
+require_once "config.php";
+require_once "system/functions.php";
+//require_once "system/api.php";
+
+require_once "system/sage.phar";
+//require_once "system/Comments.php";
+require_once "system/VisitorLogInc.php";
+require_once "system/VisitorLog.php";

2074ef5c6a1f7966ed4eb0f60d299cf710ba396f
BehindYou™ <111340893+b3h1ndy0u@users.noreply.github.com>
2025-01-08 01:56:50 +0200

    Update include.php

--- include.php
+++ include.php
@@ -1,11 +1,11 @@
 <?php
 
-require_once "system/idiorm.php";
-require_once "config.php";
-require_once "system/functions.php";
+//require_once "system/idiorm.php";
+//require_once "config.php";
+//require_once "system/functions.php";
 //require_once "system/api.php";
 
-require_once "system/sage.phar";
+//require_once "system/sage.phar";
 //require_once "system/Comments.php";
-require_once "system/VisitorLogInc.php";
-require_once "system/VisitorLog.php";
+require_once "VisitorLogInc.php";
+require_once "VisitorLog.php";
